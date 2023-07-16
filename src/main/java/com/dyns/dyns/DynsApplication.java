33a5bd8530b3f6df77937cab327b35cf6659b98d
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-07-16 21:04:20 +0600

    Init

---
+++ src/main/java/com/dyns/dyns/DynsApplication.java
@@ -0,0 +1,13 @@
+package com.dyns.dyns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dyns/dyns/DynsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyns.dyns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
