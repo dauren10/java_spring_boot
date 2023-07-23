7fe0d2f8d034c7e3c95fe42ba15f29f92e84c375
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-07-24 01:05:37 +0600

    Insert in db

---
+++ src/main/java/com/dyns/dyns/student/StudenConfig.java
@@ -0,0 +1,32 @@
+package com.dyns.dyns.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import java.util.List;
+import java.time.LocalDate;
+
+@Configuration
+public class StudenConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args ->{
+                Student mariam=  new Student(
+                	"mariam",
+                	"mariam@gmail.com",
+                	LocalDate.of(1988, 12, 10),
+                	34
+                );
+
+                Student abay=  new Student(
+                	"abay",
+                	"abayv@gmail.com",
+                	LocalDate.of(1988, 12, 10),
+                	34
+                );
+
+                repository.saveAll(List.of(mariam,abay));
+        };
+    };
+}
+
