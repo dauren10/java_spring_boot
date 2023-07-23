17d89fb75adfd29b38f6e0c41c147667885658c7
Dauren Shalabayev <dauren.shalabayev@gmail.com>
2023-07-23 19:52:11 +0600

    Studentrepository

--- src/main/java/com/dyns/dyns/student/StudentService.java
+++ src/main/java/com/dyns/dyns/student/StudentService.java
@@ -3,19 +3,29 @@ package com.dyns.dyns.student;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 @Service
 public class StudentService {
-     public List<Student> getStudents() {
-        return List.of(new Student(
-			1L,
-			"dauren",
-			"dauren.shalabayev@gmail.com",
-			LocalDate.of(1988, 12, 10),
-			34
+
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository) {
+		this.studentRepository = studentRepository;
+	}
+
+    public List<Student> getStudents() {
+        // return List.of(new Student(
+		// 	1L,
+		// 	"dauren",
+		// 	"dauren.shalabayev@gmail.com",
+		// 	LocalDate.of(1988, 12, 10),
+		// 	34
 
 
-		));
+		// ));
+		return studentRepository.findAll();
     }
 }
