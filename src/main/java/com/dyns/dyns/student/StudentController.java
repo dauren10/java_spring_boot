cfc79853c068173099c8728fa1ca39767ed4cdf3
Dauren Shalabayev <dauren10@yandex.kz>
2023-07-29 19:22:52 +0600

    Delete (#6)
    Co-authored-by: Dauren Shalabayev <dauren.shalabayev@gmail.com>

--- src/main/java/com/dyns/dyns/student/StudentController.java
+++ src/main/java/com/dyns/dyns/student/StudentController.java
@@ -4,7 +4,9 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,4 +31,11 @@ public class StudentController {
     public void registerNewStudent(@RequestBody Student student){
         studentService.addNewStudent(student);
     };
+
+    @DeleteMapping(path = "{studentId}")
+    public void deleteStudent(
+        @PathVariable("studentId") Long studentId){
+      studentService.deleteStudent(studentId);
+    };
+
 }
